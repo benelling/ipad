2b0e1e4200db8e90d30993b54bae87388ed51a59
benelling <smbt246@naver.com>
2023-06-14 16:29:55 +0900

    프로젝트 시작

---
+++ js/main.js
@@ -0,0 +1,178 @@
+import ipads from '../data/ipads.js'
+import navigations from '../data/navigations.js'
+//위처럼 import 를 쓰면 이 JS파일은 모듈화 됐다고 보며
+//이를 정상 작동 시키려면 html 파일에 type="module" 내용을 추가해야 함
+
+
+// 장바구니
+const basketStarterEl = document.querySelector('header .basket-starter')
+const basketEl = basketStarterEl.querySelector('.basket')
+
+basketStarterEl.addEventListener('click', function (event) {
+  event.stopPropagation()
+  if (basketEl.classList.contains('show')) {  // basket 클래스에 show 라는 클래스 요소가 있는지 확인
+    //hide
+    hideBasket() // basketEl.classList.remove('show') 입력해도 되지만 추상화한 함수를 호출(실행)
+  } else {
+    //show
+    showBasket() // basketEl.classList.add('show') 입력해도 되지만 추상화한 함수를 호출(실행)
+  }
+})
+basketEl.addEventListener('click', function (event) {
+  event.stopPropagation()
+})
+// 클릭이라는 이벤트 자체를 부모 요소인 basket-starter 가 알 수 없게끔 전파를 막겠다는 뜻
+// 클릭 이벤트가 전파되면 basket-starter 는 if문 ~~ 실행할 테니까 드롭다운창이 사라지게 됨
+
+window.addEventListener('click', function () {
+  hideBasket() // basketEl.classList.remove('show') 입력해도 되지만 추상화한 함수를 호출(실행)
+})
+
+function showBasket() {
+  basketEl.classList.add('show')
+}
+function hideBasket() {
+  basketEl.classList.remove('show')
+}
+
+
+// 검색
+const headerEl = document.querySelector('header')
+const headerMenuEls = [...headerEl.querySelectorAll('ul.menu > li')]
+// ... 은 '전개 연산자(Spread Operator)' 라고 함. 전개연산자를 통해 해체를 진행하고
+// 이를 대괄호로 묶어서 배열 데이터를 만듦 -> 전개연산자를 사용한 '얕은 복사(Shallow Copy)'라고 함
+// 배열 데이터로 만들면 활용 폭이 더 넓어짐
+const searchWrapEl = headerEl.querySelector('.search-wrap')
+const searchStarterEl = headerEl.querySelector('.search-starter')
+const searchCloserEl = searchWrapEl.querySelector('.search-closer')
+const searchShadowEl = searchWrapEl.querySelector('.shadow')
+const searchInputEl = searchWrapEl.querySelector('input')
+const searchDelayEls = [...searchWrapEl.querySelectorAll('li')]
+
+searchStarterEl.addEventListener('click', showSearch)
+searchCloserEl.addEventListener('click', hideSearch)
+searchShadowEl.addEventListener('click', hideSearch)
+
+
+function showSearch() {
+  headerEl.classList.add('searching')
+  document.documentElement.classList.add('fixed')
+  headerMenuEls.reverse().forEach(function (el, index) {
+    el.style.transitionDelay = index * .4 / headerMenuEls.length + 's'
+  })
+  searchDelayEls.forEach(function (el, index) {
+    el.style.transitionDelay = index * .4 / searchDelayEls.length + 's'
+  })
+  setTimeout(function () {
+    searchInputEl.focus()
+  }, 600)
+}
+function hideSearch() {
+  headerEl.classList.remove('searching')
+  document.documentElement.classList.remove('fixed')
+  headerMenuEls.reverse().forEach(function (el, index) {
+    el.style.transitionDelay = index * .4 / headerMenuEls.length + 's'
+  })
+  searchDelayEls.reverse().forEach(function (el, index) {
+    el.style.transitionDelay = index * .4 / searchDelayEls.length + 's'
+  })
+  searchDelayEls.reverse()
+  searchInputEl.value = ''
+  // 검색바 닫으면 검색내용 사라지게 초기화함
+}
+
+
+
+// 요소의 가시성 관찰
+const io = new IntersectionObserver(function (entries) {
+  entries.forEach(function (entry) {
+    if (!entry.isIntersecting) {
+      return
+    }
+    entry.target.classList.add('show')
+  })
+})
+
+const infoEls = document.querySelectorAll('.info')
+infoEls.forEach(function (el) {
+  io.observe(el)
+})
+
+
+
+// 비디오 재생
+const video = document.querySelector('.stage video')
+const playBtn = document.querySelector('.stage .controller--play')
+const pauseBtn = document.querySelector('.stage .controller--pause')
+
+playBtn.addEventListener('click', function () {
+  video.play()
+  playBtn.classList.add('hide')
+  pauseBtn.classList.remove('hide')
+})
+pauseBtn.addEventListener('click', function () {
+  video.pause()
+  playBtn.classList.remove('hide')
+  pauseBtn.classList.add('hide')
+})
+
+
+// '당신에게 맞는 iPad는?' 랜더링
+// data > ipads.js 파일 활용하여 출력
+const itemsEl = document.querySelector('section.compare .items')
+ipads.forEach(function (ipad) {
+  const itemEl = document.createElement('div')
+  itemEl.classList.add('item')
+
+  let colorList = ''
+  ipad.colors.forEach(function (color) {
+    colorList += `<li style="background-color: ${color};"></li>`
+  })
+
+  itemEl.innerHTML = /* html */ `
+  <div class="thumbnail">
+    <img src="${ipad.thumbnail}" alt="${ipad.name}" />
+  </div>
+  <ul class="colors">
+    ${colorList}
+  </ul>
+  <h3 class="name">${ipad.name}</h3>
+  <p class="tagline">${ipad.tagline}</p>
+  <p class="price">￦${ipad.price.toLocaleString('en-US')}부터</p>
+  <button class="btn">구입하기</button>
+  <a href="${ipad.url}" class="link">더 알아보기</a>
+  `
+
+  itemsEl.append(itemEl)
+})
+
+// FOOTER - Navigations 랜더링
+// data > navigations.js 파일 활용하여 출력
+const navigationsEl = document.querySelector('footer .navigations')
+navigations.forEach(function (nav) {
+  const mapEl = document.createElement('div')
+  mapEl.classList.add('map')
+
+  let mapList = ''
+  nav.maps.forEach(function (map) {
+    mapList += /* html */ `<li>
+    <a href="${map.url}">${map.name}</a>
+    </li>`
+  })
+
+  mapEl.innerHTML = /* html */ `
+    <h3>
+      <span class="text">${nav.title}</span>
+    </h3>
+    <ul>
+      ${mapList}
+    </ul>
+  `
+
+  navigationsEl.append(mapEl)
+})
+
+
+// FOOTER - copyright 연도 삽입
+const thisYearEl = document.querySelector('span.this-year')
+thisYearEl.textContent = new Date().getFullYear()
